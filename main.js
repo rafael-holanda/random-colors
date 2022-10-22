f7a4252548a11449e9f9b3fa9a600b638972913d
Rafael Holanda <59671898+rafael-holanda@users.noreply.github.com>
2022-10-22 00:13:08 +0000

    Added function that adds a random color on startup

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ function hexGenerator(){
   const chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
   var hex = "#";
   for(var i = 0; i < 6; i++){
-    hex += chars[Math.floor(Math.random() * (16 - 0))]
+    hex += chars[Math.floor(Math.random() * 16)]
   }
   return hex;
 }
 
-console.log(hexGenerator())
+function colorSet(){
+	document.getElementById("color-block").style.backgroundColor = hexGenerator();
+}
+
+colorSet();

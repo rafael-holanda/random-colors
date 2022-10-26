70a4aa5b69f843e793d7d7e748b3d349e49fdcea
rafael-holanda <p.rafael.holanda@gmail.com>
2022-10-26 13:55:30 -0300

    Hex to RBG function ready

--- main.js
+++ main.js
@@ -8,19 +8,49 @@ function hexGenerator(){
 }
 
 // Function to convert hex format to RGB format // Needs to be finished
-/* function hexToRGB(hexColor){
-  rgbColor = hexColor;
+function hexToRGB(hex){
+  var rgb = "(";
+  var red = "", green = ""; blue = "";
   for(var i = 1; i < 7; i++){
-
+    if(i == 1 || i == 2){
+      red += hex[i];
+      if(i == 2){
+        rgb += `${parseInt(red, 16)}, `
+      }
+    }
+    if(i == 3 || i == 4){
+      green += hex[i];
+      if(i == 4){
+        rgb += `${parseInt(green, 16)}, `
+      }
+    }
+    if(i == 5 || i == 6){
+      blue += hex[i];
+      if(i == 6){
+        rgb += `${parseInt(blue, 16)})`
+      }
+    }
   }
-} */
+  console.log("RGB: " + rgb);
+  return rgb;
+}
+
+// #ffffff
+// (255, 255, 255)
+
+/*
+var varHex = "ff";
+var varRgb = parseInt(varHex, 16);
+console.log(varRgb);
+*/
 
 function colorSet(){
   color = hexGenerator();
-  console.log(color);
+  console.log("HEX: " + color);
 	document.getElementById("color-block").style.backgroundColor = color;
   document.getElementById("background").style.backgroundColor = color + "50";
   document.getElementById("hex").innerHTML = `<p>${color}</p>`;
+  document.getElementById("rgb").innerHTML = `<p>${hexToRGB(color)}</p>`
 }
 
 colorSet();
